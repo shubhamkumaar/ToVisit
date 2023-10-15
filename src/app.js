6286b2a35c7a174c08f3249eb0b9e0a6f29850e7
Shubhamkumaar <shubhamkumarjhadss8@gmail.com>
2023-10-15 15:22:58 +0530

    removed node module

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const path = require("path")
 const app = express()
-// const hbs = require("hbs")
+
 const LogInCollection = require("./mongo")
 const port = process.env.PORT || 3000
 app.use(express.json())
